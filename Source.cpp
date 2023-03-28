971fa9470e12a7aeb1cc6b1f273a20d7cf135e31
NhatDoo <124895613+NhatDoo@users.noreply.github.com>
2023-03-28 14:07:03 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,19 +4,22 @@ struct Oxy
 {
 	int x;
 	int y;
+	int z;
 };
 typedef struct Oxy OXY;
 
-void nhap(OXY &oxy)
+void nhap(OXY& oxy)
 {
 	cout << "nhap hoanh do :";
 	cin >> oxy.x;
 	cout << "nhap tung do :";
 	cin >> oxy.y;
+	cout << "nhap cao do :";
+	cin >> oxy.z;
 }
-void xuat(OXY &oxy) 
+void xuat(OXY& oxy)
 {
-	cout<<" hoanh do " << oxy.x << " "<<"tung do " << oxy.y;
+	cout << " hoanh do " << oxy.x << " " << "tung do " << oxy.y << " " << " cao do" << oxy.z;
 }
 int main()
 {
